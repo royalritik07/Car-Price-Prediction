bd4e7a422fcf3edbd33782819317b93073d98fce
Hitesh Kumar <rbnritik07@gmail.com>
2024-11-02 11:40:15 +0530

    This model predicts car price

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import pickle
-import sklearn
+
 import numpy as np
 app = Flask(__name__)
 car = pd.read_csv('Cleaned_Car.csv')
